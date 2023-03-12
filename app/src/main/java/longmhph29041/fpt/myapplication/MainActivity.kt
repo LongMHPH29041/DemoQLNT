00f978c6bac999c7c2c065bf20892040f84ae136
long2 <longmhph29041@fpt.edu.vn>
2023-03-12 12:49:59 +0700

    Quan Ly Nha Tro

---
+++ app/src/main/java/longmhph29041/fpt/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package longmhph29041.fpt.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
