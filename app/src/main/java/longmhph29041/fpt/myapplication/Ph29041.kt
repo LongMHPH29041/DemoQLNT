bb4010477b25ab7b20b38966d3005eddce01bf35
long2 <longmhph29041@fpt.edu.vn>
2023-03-12 12:57:47 +0700

    Ph29041

---
+++ app/src/main/java/longmhph29041/fpt/myapplication/Ph29041.kt
@@ -0,0 +1,11 @@
+package longmhph29041.fpt.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ph29041 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ph29041)
+    }
+}
